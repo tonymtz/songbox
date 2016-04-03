ac095b10ed517cbb1f8bde9d98452359eb5eb417
tonymtz <tmartinez@switchfly.com>
2016-04-03 01:55:40 -0700

    env setup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cd ~/www/
 rm clone.tar.gz
 cat songbox.pid | xargs kill
+cp dropbox.conf ./songbox/conf/dropbox.conf
 export BEEGO_RUNMODE=prod
 nohup ./songbox/clone > songbox.log 2>&1&
 echo $! > songbox.pid
